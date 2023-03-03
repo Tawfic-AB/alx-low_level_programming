09abe925ce6bb26fb18d3b0a943d0ee18cddf6bd
Tawfic-AB <abdulbasittaufic@gmail.com>
2023-03-02 19:59:38 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@
 void print_alphabet_x10(void)
 {
 	int ten;
-	char letter;
+	char la;
 
 	for (ten = 0; ten <= 9; ten++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
 		_putchar('\n');
 	}
 }
